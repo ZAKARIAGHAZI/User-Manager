0722019d873c05ecb188d2e02145634fc4fcb7cf
ghazi <zakariaghazi77@gmail.com>
2025-08-26 16:13:22 +0100

    adding sign-in logic

--- BackEnd/controllers/auth.controller.js
+++ BackEnd/controllers/auth.controller.js
@@ -43,7 +43,37 @@ export const signUp = async (req, res, next) => {
     }
 }
 
-export const signIn = async (req, res, next) => {console.log("signin controller");};
+export const signIn = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+
+        // Check if user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            const error = new Error('Invalid email or password');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const isPasswordValide = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValide) {
+            const error = new Error('Invalid email or password');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRE });
+
+        res.status(200).json({ 
+            success: true,
+            message: 'User signed in successfully',
+            data: { token, user }
+        });
+    } catch (error) {
+        next(error);    
+    }
+};
 export const signOut = async (req, res, next) => {
     console.log("signout controller");
 };
